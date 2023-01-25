3c94709205f3598dd4f8b287199bc1153a34f56b
4strodev <jumarurex@gmail.com>
2023-01-25 19:01:41 +0100

    feat:shutdown pc

--- main.go
+++ main.go
@@ -5,34 +5,55 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 func temp() {
 	timer := time.NewTimer(time.Second * 5)
 	<-timer.C
-	fmt.Println("BOOOOOOOOOOOOOOOOOOM!")
-	os.Exit(0)
+	fmt.Println("\nBOOOOOOOOOOOOOOOOOOM!")
+	shutdown()
 }
 
 func main() {
 	go temp()
 	const key = "1"
 	nIntents := 3
+	explota := true
+
 	sc := bufio.NewReader(os.Stdin)
 
-	for i := 0; i < nIntents; i++ {
+	for ;nIntents > 0; nIntents-- {
+		fmt.Print("Introdueix la contrasenya: ")
 		text, err := sc.ReadString('\n')
 		if err != nil {
 			log.Fatal("error reading string: ", err)
 		}
 
-		if text != key {
-			fmt.Println("Contrasenya incorrecte")
-			fmt.Printf("Queden %d intents\n", i)
-			continue
+		text = strings.Trim(text, "\n\t")
+		if text == key {
+			explota = false
+			break
 		}
 
-		break
+		fmt.Println("Contrasenya incorrecte")
+		fmt.Printf("Queden %d intents\n", nIntents)
 	}
+
+	if explota {
+		shutdown()
+	}
+}
+
+func shutdown() {
+	//command := exec.Command("shutdown", "now")
+
+	//os := runtime.GOOS
+	//if os == "windows" {
+		//command = exec.Command("shutodwn", "/s")
+	//}
+
+	//command.Run()
+	fmt.Println("Apaga ordinador")
 }
